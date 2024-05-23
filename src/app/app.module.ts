fa394e502e30b4f78b690f4837764eac6d2f0f25
Sagar das <sagarmikeylevi@gmail.com>
2024-05-23 16:07:30 +0530

    add: hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { HeroSectionComponent } from './home/components/hero-section/hero-section.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
